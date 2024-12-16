275815d9ad85f97e89e3cc004b30acdea075f9eb
Arshdeep Singh <imarshdeep99@gmail.com>
2024-12-16 22:20:47 +0530

    Add Projecy

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/WeatherDashboard/WeatherDashboard";
+import City from "./pages/City/City";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function App() {
+  const queryClient = new QueryClient();
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider defaultTheme="dark">
+            <Layout>
+              <Routes>
+                <Route path="/" element={<WeatherDashboard />} />
+                <Route path="/city/:cityname" element={<City />} />
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  );
+}
+
+export default App;
